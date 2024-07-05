af9c565d5288c85c3efacdd1715d2e3d5e5364f5
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2024-07-05 17:22:16 +0530

    Update 1 Interview QnA.py

--- Python/1 Interview QnA.py
+++ Python/1 Interview QnA.py
@@ -1,3 +1,16 @@
+# You are given a dictionary with a key-value of {string: number} where values in the dictionary could be duplicates. 
+# You are required to extract the unique values from the dictionary where the value occurred only once.
+
+# Return a list of values where they occur only once.
+# Note: You can return the values in any order.
+
+# Input:
+# dictionary = {"key1": 1, "key2": 1, "key3": 7, "key4": 3, "key5": 4, "key6": 7}
+# Output:
+
+# find_unique_values(dictionary) -> [3,4]
+#Only 3 and 4 occurred once.
+
 dictionary = {"key1": 1, "key2": 1, "key3": 7, "key4": 3, "key5": 4, "key6": 7}
 
 c_hash = {}
