ebe28e40e5ed4a9268e4c0020b8e75296f630e0d
Nupoor Nawathey <nupoor01nawathey@gmail.com>
2025-01-06 11:45:02 +0530

    Update 2 Paliandrome Number.py

--- Python/2 Paliandrome Number.py
+++ Python/2 Paliandrome Number.py
@@ -52,3 +52,50 @@ print(paliandrome_string("NiTin")) # True
 print(paliandrome_string("Ratul")) # False
 
 
+'''
+125. A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+'''
+def is_paliandrome(s):
+  l, r = 0, len(s)-1
+  
+  while l < r:
+    while l < r and not is_alphanumeric(s[l]):
+      l+=1
+    while r > l and not is_alphanumeric(s[r]):
+      r+=1
+    if s[l].lower() != s[r].lower():
+      return False
+    l, r = l+1, r-1
+  return True
+    
+
+
+def is_alphanumeric(c):
+  return (
+      ord('A') <= ord(c) <= ord('Z') or 
+      ord('0') <= ord(c) <= ord('9') or
+      ord('a') <= ord(c) <= ord('z')
+  )
+  
+print(is_paliandrome("A man, a plan, a canal: Panama"))
+print(is_paliandrome("race a car"))
+print(is_paliandrome(" "))
+
